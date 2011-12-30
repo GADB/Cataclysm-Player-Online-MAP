eb013536ae1ee649f28a6ced237a6c777fa93da0
DarkAngelWoW <dark.angelwow@web.de>
2011-12-30 19:07:36 +0100

    Add More PHP FILES from WOLTK

---
+++ config/config-protected.php
@@ -0,0 +1,12 @@
+<?php
+$realmd = array(
+'db_type'         => 'mysql',
+'db_host'         => '127.0.0.1',
+'db_port'         => '3306',
+'db_username'     => 'root',
+'db_password'     => 'ascent',
+'db_name'         => 'realmd',
+'db_encoding'     => 'utf8',
+);
+$project = "trinity";
+?>
